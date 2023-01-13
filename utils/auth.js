546285a9994dd5e35c6018dc94d9b873de1167dc
RudezY <rudyyamashiro@yahoo.com>
2023-01-12 19:40:50 -0800

    initial codes

---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+//if there is no userId, the user is redirected to the login page
+//use this middleware function on any routes that require a user to be logged in to access
+
+const withAuth = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
