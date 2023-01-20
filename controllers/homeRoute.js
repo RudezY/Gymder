95896c5f86582ecaf778f1ec756d0fd04dbad038
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2023-01-19 18:32:27 -0800

    Worked on Routes, just need to know where to render Models to

---
+++ controllers/homeRoute.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const GymDataBase = require('../models/');
+const { Post, } = require('../models/Post');
+const { User } = require('../models/User');
+
+// Get all the Gym User Information, and render it to a handlebars template
+//Gets the gymDataBase specifically by the parameters
+router.get('/GymDataBase:id', async (req, res) => {
+  try {
+    const GymDataBaseData = await GymDataBase.findByAll();
+    console.log(GymDataBaseData);
+    // Find which file to render the GymDataBase into, maybe Homepage?
+    res.render('homepage', GymDataBaseData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// The below is for the postRoute that is linked to the homepage - need to find out which file it specifically belongs to
+// This calls all of the posts from the database
+router.get('/', async (req, res) => {
+  try {
+    const postData = Post.findAll();
+    const posts = postData.map((post) => post.get({ plain: true }));
+    //Is rendering into the homepage right now
+    res.render('homepage', {posts, username: 'testing'});
+  } catch (err) { /* empty */ }
+});
+
+// Make new JS file for post / user / gymDataBase to be linked to Homepage/Login/Signup
+router.get('/User', (req, res) => {
+  try {
+    const userData = User.findByPk(req.params.id);
+    console.log(userData);
+    //Rendering into the login handlebars layout
+    res.render('login', userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
